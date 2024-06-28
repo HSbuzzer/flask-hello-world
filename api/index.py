c4e3cc0555117471937dc0980bf4a63316bb3c34
FlagrateAE <shadowclipyt@gmail.com>
2024-06-28 15:46:59 +0300

    Add counter

--- api/index.py
+++ api/index.py
@@ -5,10 +5,12 @@ from flask_cors import CORS
 app = flask.Flask(__name__)
 CORS(app)
 
+counter = 0
+
 
 @app.route("/")
 def home():
-    resp = flask.Response("Hello world!")
+    resp = flask.Response(f"Hello world! {counter} times")
     resp.headers["Content-Type"] = "text/plain"
     resp.headers["Connection"] = "Keep-Alive"
     resp.headers["Keep-Alive"] = "timeout=5, max=1000"
