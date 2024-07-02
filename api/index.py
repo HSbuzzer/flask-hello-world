d7fe4a8042f7cd83d42f6ad026b11b6bffdc51c9
FlagrateAE <shadowclipyt@gmail.com>
2024-07-02 21:01:45 +0300

    led system

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from requests import get
 from dotenv import load_dotenv
 import os
-from time import sleep
+import json
 
 load_dotenv()
 app = flask.Flask(__name__)
@@ -25,14 +25,33 @@ def home():
 
 @app.route("/LED")
 def LED():
-    botUpdates = get(f'https://api.telegram.org/bot{os.getenv("BOT_TOKEN")}/getUpdates')
-    botUpdates = botUpdates.json()
-    return str(botUpdates)
+    # load last updateID
+    with open('api/lastUpdate.json', 'r') as f:
+        last_update = json.load(f)
+    updateID = last_update['update_id'] + 1
+    
+    
+    botUpdates = get(f'https://api.telegram.org/bot{os.getenv("BOT_TOKEN")}/getUpdates?allowed_updates=message&timeout=59&limit=1&offset={updateID}')
+    update = botUpdates.json()['result']
+    
+    if update:
+        # write to lastUpdate.json to "update_id" field
+        writable = {
+            "update_id": update[-1]["update_id"],
+            "color": update[-1]["message"]["text"].replace("/color ", "")
+        }
+        with open('api/lastUpdate.json', 'w') as f:
+            json.dump(writable, f)
+            
+        return writable["color"]
+    
+    else:
+        return "no updates found"
+    
 
 @app.route("/test")
 def test():
     command = f"https://api.telegram.org/bot{os.getenv('BOT_TOKEN')}/sendMessage?chat_id={os.getenv('CHAT_ID')}&text=operational"
-    print(command)
     get(command)
     return f"message sent to chat {os.getenv('CHAT_ID')}"
 
